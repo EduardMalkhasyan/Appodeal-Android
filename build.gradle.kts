07d0d674b8905ec10827794e103c30028a29da60
Eduard Malkhasyan <eduardmalkhasyan@gmail.com>
2025-04-02 20:11:49 +0400

    w

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,3 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
 }
 
-// For top-level build.gradle, you typically don't put implementation dependencies here
-// Instead, you might want to define dependency versions or common configurations
-subprojects {
-    afterEvaluate {
-        dependencies {
-            // Use single quotes or double quotes consistently
-            implementation 'com.appodeal.ads:sdk:3.5.1.0'
-        }
-    }
-}
